0e0f8f28d395f66f4ea79e09da7ca8bf1c752e12
iiVladIii <vladislav2407@icloud.com>
2025-10-13 12:30:19 +0300

    css modules support

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,29 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./src/setupTests.ts'],
-    },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './src'),
+export default defineConfig((config) => {
+    const isDev = config.mode === 'development';
+
+    return {
+        plugins: [react()],
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: ['./src/setupTests.ts'],
         },
-    },
+        css: {
+            modules: {
+                localsConvention: 'camelCase',
+                generateScopedName: isDev
+                    ? '[path][name]__[local]--[hash:base64:5]'
+                    : '[hash:base64:8]',
+                exportGlobals: false,
+            },
+        },
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, './src'),
+            },
+        },
+    };
 });
