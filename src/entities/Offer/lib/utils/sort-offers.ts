6ca3c3d72205a9ed41da125f8717f752917c54c2
iiVladIii <vladislav2407@icloud.com>
2025-10-20 22:56:50 +0300

    sort by popular is default server sort

--- src/entities/Offer/lib/utils/sort-offers.ts
+++ src/entities/Offer/lib/utils/sort-offers.ts
@@ -6,11 +6,7 @@ export const sortOffers = (
     sortType: OFFER_SORT_TYPE,
 ): Offer[] => {
     const sortFunctions = {
-        'popular-desc': (a: Offer, b: Offer) => {
-            if (a.isPremium !== b.isPremium) return b.isPremium ? 1 : -1;
-            if (a.isFavorite !== b.isFavorite) return b.isFavorite ? 1 : -1;
-            return b.rating - a.rating;
-        },
+        'popular-desc': (a: Offer, b: Offer) => 0,
         'price-asc': (a: Offer, b: Offer) => a.price - b.price,
         'price-desc': (a: Offer, b: Offer) => b.price - a.price,
         'top-desc': (a: Offer, b: Offer) => b.rating - a.rating,
