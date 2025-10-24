9bd48a10cab635dc9fc549b14ed8e49566074882
iiVladIii <vladislav2407@icloud.com>
2025-10-24 19:10:10 +0300

    removed props

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -1,23 +1,15 @@
 import { memo } from 'react';
 
-interface HeaderProps {
-    className?: string;
-}
-
-export const Footer = memo((props: HeaderProps) => {
-    const { className: _className } = props;
-
-    return (
-        <footer className="footer container">
-            <a className="footer__logo-link" href="main.html">
-                <img
-                    className="footer__logo"
-                    src="img/logo.svg"
-                    alt="6 cities logo"
-                    width="64"
-                    height="33"
-                />
-            </a>
-        </footer>
-    );
-});
+export const Footer = memo(() => (
+    <footer className="footer container">
+        <a className="footer__logo-link" href="main.html">
+            <img
+                className="footer__logo"
+                src="img/logo.svg"
+                alt="6 cities logo"
+                width="64"
+                height="33"
+            />
+        </a>
+    </footer>
+));
