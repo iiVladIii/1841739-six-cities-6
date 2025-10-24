4ac4eacda4fb3c319ef5884a33b9b66cf2b75de4
iiVladIii <vladislav2407@icloud.com>
2025-10-24 19:40:47 +0300

    fix sort-by data refetch

--- src/features/city-tabs/ui/city-page-tabs/city-page-tabs.tsx
+++ src/features/city-tabs/ui/city-page-tabs/city-page-tabs.tsx
@@ -4,24 +4,38 @@ import { getRouteMainPage } from '@/shared/consts/router';
 import cn from 'classnames';
 import { CITY_NAME, cityActions, useCityName } from '@/entities/city';
 import { useAppDispatch } from '@/shared/hooks/use-app-dispatch';
+import { useLinkWithParams } from '@/shared/hooks/use-navigate-with-params';
 
 export const CityPageTabs = memo(() => {
     const cities = Object.values(CITY_NAME);
     const city = useCityName();
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const { setCity, resetCity } = cityActions;
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
+    const getRouteLink = useLinkWithParams();
 
     useEffect(() => {
+        const defaultLink = getRouteLink(getRouteMainPage(), {
+            city: CITY_NAME.Paris,
+            'sort-by': 'popular-desc',
+        });
         if (searchParams) {
             const cityParam = searchParams.get('city');
             if (cityParam) dispatch(setCity(cityParam));
-            else navigate(`${getRouteMainPage()}?city=${CITY_NAME.Paris}`);
+            else navigate(defaultLink);
         } else {
-            navigate(`${getRouteMainPage()}?city=${CITY_NAME.Paris}`);
+            navigate(defaultLink);
         }
-    }, [dispatch, navigate, searchParams, setCity]);
+    }, [dispatch, getRouteLink, navigate, searchParams, setCity]);
+
+    useEffect(() => {
+        const sortByParam = searchParams.get('sort-by');
+        const defaultLink = getRouteLink(getRouteMainPage(), {
+            'sort-by': 'popular-desc',
+        });
+        if (!sortByParam) navigate(defaultLink);
+    }, [getRouteLink, navigate, searchParams]);
 
     useEffect(
         () => () => {
@@ -29,16 +43,6 @@ export const CityPageTabs = memo(() => {
         },
         [],
     );
-
-    useEffect(() => {
-        if (city && !searchParams.get('city')) {
-            setSearchParams((prev) => {
-                const newParams = new URLSearchParams(prev);
-                newParams.set('city', city);
-                return newParams;
-            });
-        }
-    }, [city, searchParams, setSearchParams]);
 
     return (
         <div className="tabs">
@@ -52,7 +56,9 @@ export const CityPageTabs = memo(() => {
                                     'tabs__item',
                                     { 'tabs__item--active': city === _c },
                                 )}
-                                to={`${getRouteMainPage()}?city=${_c}`}
+                                to={getRouteLink(getRouteMainPage(), {
+                                    city: _c,
+                                })}
                             >
                                 <span>{_c}</span>
                             </Link>
